80a82a3fc1ce24a8d09fbcc64561e2981146fe44
zinedinealvarez <zinedinealvarezsais@gmail.com>
2026-07-04 21:43:16 +0200

    Kubernetes missconfig

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const controlledSemgrepTest = () => eval('1 + 1');
-
 app.get('/', (req, res) => {
   res.json({
     solution: 'SecureKubeOps',
